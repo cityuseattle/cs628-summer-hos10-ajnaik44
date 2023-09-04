9ee4720a9e093039735f4388a16c72c523b1311d
Ajay <ajay.naik44@gmail.com>
2023-09-04 04:11:36 +0000

    HOS10 Ajay Submission

---
+++ frontend/src/components/login.js
@@ -0,0 +1,13 @@
+//HOS10A login.js
+import React from "react";
+
+const Login = () => {
+  return (
+    <a href="https://jubilant-cod-69wxpvp64vp245wr-5050.app.github.dev/facebook" 
+    className="btn btn-primary">
+      Login with Facebook
+    </a>
+  );
+};
+
+export default Login;
